017eda44edc51d75b1c4275cd216b530159fc90f
yaseenkhamissa-prog <yaseenkhamissa@icloud.com>
2025-12-30 12:05:23 +0500

    Implement game settings handler with preference logic

--- api/game-settings/index.js
+++ api/game-settings/index.js
@@ -1,1 +1,25 @@
+export default function handler(req, res) {
+  const name = (req.query.name || "").toLowerCase();
 
+  const rightHudGames = ["fortnite", "apex", "pubg", "overwatch"];
+  const leftHudGames = [
+    "valorant",
+    "counter strike",
+    "csgo",
+    "cs2",
+    "call of duty",
+    "warzone"
+  ];
+
+  let preferSide = "right";
+
+  if (leftHudGames.some(g => name.includes(g))) preferSide = "left";
+  if (rightHudGames.some(g => name.includes(g))) preferSide = "right";
+
+  res.status(200).json({
+    game: name || "unknown",
+    preferSide,
+    keepW: 0.5,
+    keepH: 0.45
+  });
+}
